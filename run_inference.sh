2d5cc1790ce59f5c13b9142a4d30e19a4988dd52
CHELSEA234 <xiao07guo@gmail.com>
2024-12-05 13:22:20 -0500

    remove entrance

--- run_inference.sh
+++
@@ -1,7 +0,0 @@
-source ~/.bashrc
-conda activate RED
-CUDA_NUM=6
-CUDA_VISIBLE_DEVICES=$CUDA_NUM python inference.py --cross_val=1 --batch_size=256 --protocol=Non --lr=0.1
-CUDA_VISIBLE_DEVICES=$CUDA_NUM python inference.py --cross_val=2 --batch_size=256 --protocol=Non --lr=0.1
-CUDA_VISIBLE_DEVICES=$CUDA_NUM python inference.py --cross_val=3 --batch_size=256 --protocol=Non --lr=0.1
-CUDA_VISIBLE_DEVICES=$CUDA_NUM python inference.py --cross_val=4 --batch_size=256 --protocol=Non --lr=0.1
